6c567aed38658e3efc4cbd43c5bd018d795bfac2
家民 <zjm950207@iCloud.com>
2018-07-07 14:57:56 +0800

    删除

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,17 +6,21 @@ import VueRouter from 'vue-router';
 import HeroList from '../views/heroes/List.vue';
 import WeaponList from '../views/weapons/List.vue';
 import EquipList from '../views/equips/List.vue';
+import AddHeros from '../views/heroes/Add.vue'
 
 // 注册插件
 Vue.use(VueRouter);
 
 // 2. 创建路由对象，配置路由规则
 const router = new VueRouter({
+  linkExactActiveClass: 'active',
   routes: [
+    { name: 'home', path: '/', redirect: {name: 'heroes'} },
     // 路由规则
     { name: 'heroes', path: '/heroes', component: HeroList },
     { name: 'weapons', path: '/weapons', component: WeaponList },
-    { name: 'equips', path: '/equips', component: EquipList }
+    { name: 'equips', path: '/equips', component: EquipList },
+    { name: 'heroadd', path: '/heroes/add', component: AddHeros}
   ]
 });
 
